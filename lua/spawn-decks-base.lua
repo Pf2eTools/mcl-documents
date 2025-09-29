0d6f37f048848db52c710a4425ebe56b5954ad94
Pf2eTools <78115075+Pf2eTools@users.noreply.github.com>
2025-09-29 23:13:20 +0200

    loyalist targs

--- lua/spawn-decks-base.lua
+++ lua/spawn-decks-base.lua
@@ -1,8 +1,7 @@
 local Spawner = require("lua.spawner")
-local decks = require("lua.decks")
 
 function onClickSpawn()
-    print("Spawning MCL decks...")
+    print("Spawning decks...")
     local spawner = Spawner()
     local position = self.positionToWorld({0, 0, 0}) + Vector({0, 0, 1})
     for _, deck in ipairs(decks) do
@@ -21,11 +20,11 @@ function onLoad()
     self.createButton({
         click_function = "onClickSpawn",
         function_owner = self,
-        label = "Spawn\nMCL Decks",
+        label = "Spawn\nDecks",
         position = Vector(0, 0.1, 0),
         width = 1500,
         height = 1500,
         font_size = 300,
-        tooltip = "Click to spawn MCL decks!",
+        tooltip = "Click to spawn decks!",
     })
 end
